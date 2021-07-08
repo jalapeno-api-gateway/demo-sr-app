78380826a719dbee1f27f357f9dd1a678aa63faa
Michel Bongard <michel_bongard@hotmail.com>
2021-07-08 21:30:00 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gitlab.ost.ch/ins/jalapeno-api/sr-app
+
+go 1.16
+
+require (
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+	google.golang.org/grpc v1.39.0
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	rsproto "gitlab.ost.ch/ins/jalapeno-api/sr-app/request-service"
+)
+
+func StartApp() {
+	log.Print("Starting SR-App ...")
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	client := rsproto.NewApiGatewayClient(conn)
+	ids := []int32{1, 2, 5, 10}
+	message := &rsproto.NodeIds{Ids: ids}
+	stream, err := client.GetNodes(context.Background(), message)
+	if err != nil {
+		log.Fatalf("Error when calling GetNodes on RequestService: %s", err)
+	}
+
+	for {
+		node, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetNodes(_) = _, %v", client, err)
+		}
+		log.Println(node)
+	}
+	log.Printf("---------------------All Nodes received---------------------")
+}
