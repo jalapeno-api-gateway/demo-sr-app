b86af3f662ec067b308985c7306e8ec27452ff81
DILLI <illi.dominique.v@gmail.com>
2021-07-16 09:47:17 +0200

    refactored GetNodes into method

--- main.go
+++ main.go
@@ -32,6 +32,28 @@ func GetDataRates(client rsproto.ApiGatewayClient) {
 	log.Printf("---------------------All DataRates received---------------------")
 }
 
+func GetNodes(client rsproto.ApiGatewayClient) {
+	log.Printf("---------------------Requesting Nodes---------------------")
+	ids := []string{"2_0_0_0000.0000.000a", "2_0_0_0000.0000.0001", "2_0_0_0000.0000.000c"}
+	message := &rsproto.NodeIds{Ids: ids}
+	stream, err := client.GetNodes(context.Background(), message)
+	if err != nil {
+		log.Fatalf("Error when calling GetNodes on RequestService: %s", err)
+	}
+
+	for {
+		node, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetNodes(_) = _, %v", client, err)
+		}
+		log.Println(node)
+	}
+	log.Printf("---------------------All Nodes received---------------------")
+}
+
 func main() {
 	log.Print("Starting SR-App ...")
 	var conn *grpc.ClientConn
@@ -42,24 +64,7 @@ func main() {
 	defer conn.Close()
 
 	client := rsproto.NewApiGatewayClient(conn)
-	// ids := []string{"2_0_0_0000.0000.000a", "2_0_0_0000.0000.0001", "2_0_0_0000.0000.000c"}
-	// message := &rsproto.NodeIds{Ids: ids}
-	// stream, err := client.GetNodes(context.Background(), message)
-	// if err != nil {
-	// 	log.Fatalf("Error when calling GetNodes on RequestService: %s", err)
-	// }
 
-	// for {
-	// 	node, err := stream.Recv()
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	if err != nil {
-	// 		log.Fatalf("%v.GetNodes(_) = _, %v", client, err)
-	// 	}
-	// 	log.Println(node)
-	// }
-	// log.Printf("---------------------All Nodes received---------------------")
-
+	GetNodes(client)
 	GetDataRates(client)
 }
