745ac62ece55ff1734a063ceb2351c3ed6a50f2c
Michel Bongard <michel_bongard@hotmail.com>
2021-07-20 20:55:07 +0200

    push poc

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"google.golang.org/grpc"
 
 	rsproto "gitlab.ost.ch/ins/jalapeno-api/sr-app/proto/request-service"
+	psproto "gitlab.ost.ch/ins/jalapeno-api/sr-app/proto/push-service"
 )
 
 func GetDataRates(client rsproto.ApiGatewayClient) {
@@ -30,6 +31,27 @@ func GetDataRates(client rsproto.ApiGatewayClient) {
 		log.Println(dataRate)
 	}
 	log.Printf("---------------------All DataRates received---------------------")
+}
+
+func SubscribeToDataRates(client psproto.PushServiceClient) {
+	log.Printf("---------------------Subscribing to DataRates---------------------")
+	ips := []string{"10.18.8.53", "10.18.8.54"}
+	message := &psproto.IPv4Addresses{Ipv4Address: ips}
+	stream, err := client.SubscribeToDataRates(context.Background(), message)
+	if err != nil {
+		log.Fatalf("Error when calling SubscribeToDataRates on PushService: %s", err)
+	}
+	for {
+		dataRate, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetDataRates(_) = _, %v", client, err)
+		}
+		log.Println(dataRate)
+	}
+	log.Printf("--------------------- Subscription ended ---------------------")
 }
 
 func GetNodes(client rsproto.ApiGatewayClient) {
@@ -56,15 +78,27 @@ func GetNodes(client rsproto.ApiGatewayClient) {
 
 func main() {
 	log.Print("Starting SR-App ...")
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(os.Getenv("REQUEST_SERVICE_ADDRESS"), grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Could not connect: %s", err)
+
+	//Connecting to Request Service
+	var rsConn *grpc.ClientConn
+	rsConn, rsErr := grpc.Dial(os.Getenv("REQUEST_SERVICE_ADDRESS"), grpc.WithInsecure())
+	if rsErr != nil {
+		log.Fatalf("Could not connect to request service: %s", rsErr)
 	}
-	defer conn.Close()
+	defer rsConn.Close()
 
-	client := rsproto.NewApiGatewayClient(conn)
+	//Connecting to Push Service
+	var psConn *grpc.ClientConn
+	psConn, psErr := grpc.Dial(os.Getenv("PUSH_SERVICE_ADDRESS"), grpc.WithInsecure())
+	if psErr != nil {
+		log.Fatalf("Could not connect to push service: %s", psErr)
+	}
+	defer psConn.Close()
 
-	GetNodes(client)
-	GetDataRates(client)
+	rsClient := rsproto.NewApiGatewayClient(rsConn)
+	psClient := psproto.NewPushServiceClient(psConn)
+
+	GetNodes(rsClient)
+	GetDataRates(rsClient)
+	SubscribeToDataRates(psClient)
 }
